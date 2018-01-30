45147bf75c4fa13b6a595f8608611221af9ce3bd
cerealchan <35574138+cerealchan@users.noreply.github.com>
2018-01-30 04:00:48 -0800

    create script.js

---
+++ studio1/script.js
@@ -0,0 +1,26 @@
+'use strict';
+console.log("Hello!");
+
+
+//document form submission
+document.madlibs.onsubmit = processForm;
+
+//document the clearing of the form
+document.madlibs.onreset = clearForm;
+
+var submission = document.getElementById("submission");
+var myMsg = document.getElementById("myMsg");
+
+function processForm() {
+    console.log("Submitted!");
+
+    //capture the form field responses as vars, too
+    var name = document.madlibs.name.value;
+    var soft = document.madlibs.soft.value;
+    var adjective = document.madlibs.adjective.value;
+    var positive = document.madlibs.positive.value;
+
+    myMsg.innerHTML = "Hi" + name + "You are as kind as a" + adjective + soft + "and just as" + positive + "too! You are awesome!"
+
+    return false;
+}
